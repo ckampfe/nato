112268cf8deb3a0309d756cc6cb959c349eae37e
Clark Kampfe <clark.kampfe@gmail.com>
2025-10-27 22:04:50 -0500

    no binary search, direct lookup

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,35 @@
-static MAPPING: [(u8, &str); 26] = [
-    (b'a', "Alfa"),
-    (b'b', "Bravo"),
-    (b'c', "Charlie"),
-    (b'd', "Delta"),
-    (b'e', "Echo"),
-    (b'f', "Foxtrot"),
-    (b'g', "Golf"),
-    (b'h', "Hotel"),
-    (b'i', "India"),
-    (b'j', "Juliett"),
-    (b'k', "Kilo"),
-    (b'l', "Lima"),
-    (b'm', "Mike"),
-    (b'n', "November"),
-    (b'o', "Oscar"),
-    (b'p', "Papa"),
-    (b'q', "Quebec"),
-    (b'r', "Romeo"),
-    (b's', "Sierra"),
-    (b't', "Tango"),
-    (b'u', "Uniform"),
-    (b'v', "Victor"),
-    (b'w', "Whiskey"),
-    (b'x', "Xray"),
-    (b'y', "Yankee"),
-    (b'z', "Zulu"),
+static MAPPING: [&str; 26] = [
+    "Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett",
+    "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango",
+    "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu",
 ];
+
+fn map_to_nato(input: &str) -> String {
+    let input = input.to_lowercase();
+
+    input
+        .bytes()
+        .filter_map(|b| MAPPING.get((b - b'a') as usize).copied())
+        .collect::<Vec<&str>>()
+        .join(" ")
+}
 
 fn main() {
     std::env::args().skip(1).for_each(|input| {
-        let input = input.to_lowercase();
-
-        let words = input
-            .bytes()
-            .map(|b| {
-                MAPPING
-                    .binary_search_by_key(&b, |(t, _s)| *t)
-                    .map(|i| MAPPING[i].1)
-                    .expect("inputs must be [A-Za-z]")
-            })
-            .collect::<Vec<&str>>()
-            .join(" ");
-
+        let words = map_to_nato(&input);
         println!("{words}");
     });
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::map_to_nato;
+
+    #[test]
+    fn it_does_it() {
+        assert_eq!(
+            map_to_nato("hello"),
+            "Hotel Echo Lima Lima Oscar".to_string()
+        );
+    }
 }
